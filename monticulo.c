dd122b807ccf47b30dad32af6d0f98ce74a17d2e
alex <alejandro.fernandezo@udc.es>
2022-11-17 11:52:32 +0100

    monticulos

---
+++ monticulo.c
@@ -0,0 +1,5 @@
+//
+// Created by alex on 17/11/22.
+//
+
+#include "monticulo.h"
---
+++ monticulo.h
@@ -0,0 +1,8 @@
+//
+// Created by alex on 17/11/22.
+//
+
+#ifndef P4_MONTICULO_H
+#define P4_MONTICULO_H
+
+#endif //P4_MONTICULO_H
